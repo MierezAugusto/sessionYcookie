ff2de5b43af851b3164df2c191b574876396f310
MierezAugusto <augustomierez@gmail.com>
2022-02-03 21:28:56 -0300

    repositorio sessionYcookie

---
+++ src/middleware/validate.js
@@ -0,0 +1,16 @@
+const{check} = require ("express-validator")
+
+module.exports = [
+    check("name")
+    .notEmpty().withMessage("Nombre requerido"),
+
+    check("email")
+    .notEmpty().withMessage("Email requerido").bail()
+    .isEmail().withMessage("Debe ingresar un mail valido"),
+
+    check("color")
+    .notEmpty().withMessage("Color requerido"),
+    
+    check("edad")
+    .isInt().withMessage("La edad debe ser un número")
+]
